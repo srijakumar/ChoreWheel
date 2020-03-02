3925bcea4e5444824e30dca2378f413b60f134b8
srijakumar <srijakumar7@gmail.com>
2020-03-02 12:45:02 -0500

    Added style for mark as complete methods and built methods to ensure it works in task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :chore
+
+  def completed?
+    !completed_at.blank? #if time is not present, then task not completed. re method complete in task controller
+  end
 end
